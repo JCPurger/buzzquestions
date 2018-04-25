09944e8fdbb866b09c447869ace7c62874715221
Unknown <jc.purger@gmail.com>
2018-04-25 18:20:50 -0300

    migrations database

---
+++ database/migrations/2018_04_20_194101_create_answer_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');
+            $table->unsignedInteger('question_id');
+
+            $table->foreign('question_id')->references('id')->on('question')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answer', function (Blueprint $table) {
+            $table->dropForeign('question_id');
+        });
+        Schema::dropIfExists('answer');
+    }
+}
