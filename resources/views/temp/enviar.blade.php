56f2e51716c00c6a5bbbe256328a5311b4c75b74
Amanda <arpuga95@gmail.com>
2018-07-08 20:33:33 -0300

    Enviar.2
    corrigindo as correções

--- resources/views/temp/enviar.blade.php
+++ resources/views/temp/enviar.blade.php
@@ -7,108 +7,123 @@
   <div class="card">
 
     <form action="/" method="post">
+      <h3>Compartilhe seu questionário!</h3>
       <div class="col-sm-10">
         <div class="row send">
-          <h3>Compartilhe seu questionário!</h3>
+
           <div class="form-group">
             <label>Enviar para:</label>
-            <input type="text" class="form-control form-enviar" name="email" id="email" placeholder="email@exemplo.com">
+            <div class="row">
+              <div class="col-md-4 offset-md-2">
+                <input type="text" class="form-control form-enviar" name="email" id="email" placeholder="email@exemplo.com">
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </form>
 
-    <hr>
+      <hr>
 
-    <form action="/" method="post">
       <div class="col-sm-10">
         <div class="row send">
           <div class="form-group">
             <label>Assunto:</label>
-            <input type="text" class="form-control form-enviar" name="email" id="email" placeholder="Queremos ouvir sua opinião">
+            <div class="row">
+              <div class="col-md-4 offset-md-2">
+                <input type="text" class="form-control form-enviar" name="email" id="email" placeholder="Queremos ouvir sua opinião">
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </form>
 
-    <hr>
 
-    <label>Mensagem:</label>
-    <table>
-      <tbody>
-        <tr>
-          <td align="center" class="env_mens">
-            <table class="table-mens" cellpadding="5" cellspacing="5">
-              <tbody>
-                <tr>
-                  <td class="td-mens">
-                    <table class="table-td-mens">
-                      <tbody>
-                        <tr>
-                          <td class="table-td-table">
-                            <table width="100%" class="table-td-table">
-                              <tbody>
-                                <tr>
-                                  <td>
-                                    <h2><a class="a-span" target="_blank">Nome do Questionário
-                                    </span></a>
-                                  </h2>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <table class="table-td-mens">
+      <hr>
+
+      <label>Mensagem:</label>
+      <div class="row">
+        <div class="col-md-4 offset-md-4">
+          <table>
+            <tbody>
+              <tr>
+                <td align="center" class="env_mens">
+                  <table class="table-mens" cellpadding="5" cellspacing="5">
                     <tbody>
                       <tr>
-                        <td class="table-td-table">
-                          <table width="100%" class="table-td-table">
+                        <td class="td-mens">
+                          <table class="table-td-mens">
                             <tbody>
                               <tr>
                                 <td class="table-td-table">
-                                  <div class="mens-body">
-                                    <div class="span-mens">Este é um questionário de pesquisa e sua participação é importante. Clique no botão abaixo para começar a respondê-lo. Agradecemos sua
-                                      participação!<span class="span-mens-span"></span>.
-                                    </div>
-                                  </div>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <table class="table-td-mens">
-                    <tbody>
-                      <tr>
-                        <td class="table-td-table">
-                          <table class="table-bot">
-                            <tbody class="span-mens">
-                              <tr class="span-mens">
-                                <td align="center" class="td-bot"><a herf="" target="_blank" class="a-span">Iniciar Questionário</a>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table> 
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-  <button type="submit" class="button button-block"/>
+                                  <table width="100%" class="table-td-table">
+                                    <tbody>
+                                      <tr>
+                                        <td>
+                                          <h2><a class="a-span" target="_blank">Nome do Questionário
+                                          </span></a>
+                                        </h2>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <table class="table-td-mens">
+                          <tbody>
+                            <tr>
+                              <td class="table-td-table">
+                                <table width="100%" class="table-td-table">
+                                  <tbody>
+                                    <tr>
+                                      <td class="table-td-table">
+                                        <div class="mens-body">
+                                          <div class="span-mens .col-md-6 .offset-md-3">Este é um questionário de pesquisa e sua participação é importante. Clique no botão abaixo para começar a respondê-lo. Agradecemos sua
+                                            participação!
+                                          </div>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <table class="table-td-mens">
+                  <tbody>
+                    <tr>
+                      <td class="table-td-table">
+                        <table class="table-bot">
+                          <tbody class="span-mens">
+                            <tr class="span-mens">
+                              <td align="center" class="td-bot"><a herf="" target="_blank" class="a-span">Iniciar Questionário</a>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table> 
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</div>
+<button type="submit" class="button button-block"/>
 Enviar</button>
-
+</form>
 </div>
 </header>
 
